bee10ffe21f017648f0d3c1ca0e92cab10058187
facundo1000 <martinez.facundo85@gmail.com>
2025-05-06 19:48:26 -0300

    add:SecurityConfig para evitar problemas en rutas

--- alkywallet/src/main/java/org/alkemy/alkywallet/config/SecurityConfig.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/config/SecurityConfig.java
@@ -1,7 +1,42 @@
 package org.alkemy.alkywallet.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Security configuration class
+ * <br>
+ * Autor: Squad2
+ */
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    /**
+     * Security filter chain configuration
+     * @param http
+     * @return HttpSecurity object
+     * @throws Exception
+     */
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .build();
+    }
 }
