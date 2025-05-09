3888488bd5c59514097b3781588c7fc930a38a8e
facundo1000 <martinez.facundo85@gmail.com>
2025-05-09 17:01:42 -0300

    feat: dto's para auth

---
+++ alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/AuthResponse.java
@@ -0,0 +1,7 @@
+package org.alkemy.alkywallet.controllers.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"username", "message", "jwt", "status"})
+public record AuthResponse(String username, String message, String jwt, boolean status) {
+}
