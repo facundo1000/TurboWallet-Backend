da329be95e022ddf3c0b9389e08432a80a9fb75f
facundo1000 <martinez.facundo85@gmail.com>
2025-05-18 02:49:38 -0300

    add: msg de error

--- alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/AuthLoginRequest.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/AuthLoginRequest.java
@@ -3,9 +3,10 @@ package org.alkemy.alkywallet.controllers.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-public record AuthLoginRequest(@NotBlank
+public record AuthLoginRequest(@NotBlank(message = "ingresar un email valido")
                                @Schema(description = "user email", example = "juan.perez@example.com")
                                String username,
                                @Schema(description = "user password", example = "1234")
-                               @NotBlank String password) {
+                               @NotBlank(message = "ingrese un password valido")
+                               String password) {
 }
