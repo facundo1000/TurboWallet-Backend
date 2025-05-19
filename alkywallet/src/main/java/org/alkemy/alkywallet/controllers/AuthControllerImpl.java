ba5fb9c4550de122edca44c5f3f78883b6fac718
facundo1000 <martinez.facundo85@gmail.com>
2025-05-19 08:46:11 -0300

    add: crossOrigin annotation

--- alkywallet/src/main/java/org/alkemy/alkywallet/controllers/AuthControllerImpl.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/controllers/AuthControllerImpl.java
@@ -11,13 +11,11 @@ import org.alkemy.alkywallet.swagger.save.ApiResponseCrear;
 import org.alkemy.alkywallet.swagger.save.ApiResponseUsuarioLogin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:5173"})
 @RequestMapping("/api/v1/auth")
 @Tag(name = "AuthController", description = "Controlador de login y registro de usuarios")
 public class AuthControllerImpl {
