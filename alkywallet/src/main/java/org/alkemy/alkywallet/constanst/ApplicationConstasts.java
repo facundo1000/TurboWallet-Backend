bde638a86beb1c62549f7058bc433d8f93f605ff
facundo1000 <martinez.facundo85@gmail.com>
2025-05-23 18:08:43 -0300

    add: constants class

---
+++ alkywallet/src/main/java/org/alkemy/alkywallet/constanst/ApplicationConstasts.java
@@ -0,0 +1,6 @@
+package org.alkemy.alkywallet.constanst;
+
+public class ApplicationConstasts {
+    public static final String BASE_URL = "http://localhost:8080/api/v1/";
+    public static final String PASS_REGEX = "^[A-Za-z0-9!@#$%^&*()_+\\-=\\[\\]{};':|,.<>\\/?]{4,25}$";
+}
