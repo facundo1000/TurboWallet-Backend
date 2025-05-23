ce6bb5dc2f9bf8095af8eaf70f74b704eaf3e8a7
facundo1000 <martinez.facundo85@gmail.com>
2025-05-23 12:32:22 -0300

    add: user response

--- alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/AuthResponse.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/AuthResponse.java
@@ -2,6 +2,6 @@ package org.alkemy.alkywallet.controllers.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"username", "message", "jwt", "status"})
-public record AuthResponse(String username, String message, String jwt, boolean status) {
+@JsonPropertyOrder({"username", "message", "jwt", "status","id"})
+public record AuthResponse(String username, String message, String jwt, boolean status, Long id) {
 }
