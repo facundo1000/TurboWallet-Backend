9699550bb94fa817dc0dff8d20fc316238908d6e
facundo1000 <martinez.facundo85@gmail.com>
2025-05-23 11:28:28 -0300

    add: dato tipoTarjeta

--- alkywallet/src/main/resources/data.sql
+++ alkywallet/src/main/resources/data.sql
@@ -26,11 +26,11 @@ INSERT INTO tbl_usuario_cuentas (id_cuenta,id_usuario) VALUES (5,1);
 
 
 INSERT INTO tarjetas (nombre_titular,tipo, fecha_vencimiento, estado, tope_gasto, numero_tarjeta, banco, cvv) VALUES
-('roberto carlos','credito', '2026-07-01', true, 10000.00, '4111111111111111', 'Banco Uno', '123'),
-('marcelo gallardo','debito', '2025-12-01', true, 3000.00, '4222222222222', 'Banco Dos', '456'),
-('juan manso','credito', '2024-10-01', false, 2000.00, '4333333333333', 'Banco Tres', '789'),
-('obi juan','debito', '2026-01-01', true, 1500.00, '4444444444444', 'Banco Uno', '321'),
-('guido','credito', '2027-05-01', true, 5000.00, '4555555555555', 'Banco Cuatro', '654');
+('roberto carlos','CREDITO', '2026-07-01', true, 10000.00, '4111111111111111', 'Banco Uno', '123'),
+('marcelo gallardo','NATIVA', '2025-12-01', true, 3000.00, '4222222222222', 'Banco Dos', '456'),
+('juan manso','CREDITO', '2024-10-01', false, 2000.00, '4333333333333', 'Banco Tres', '789'),
+('obi juan','DEBITO', '2026-01-01', true, 1500.00, '4444444444444', 'Banco Uno', '321'),
+('guido','CREDITO', '2027-05-01', true, 5000.00, '4555555555555', 'Banco Cuatro', '654');
 
 INSERT INTO tbl_cuenta_tarjeta(id_cuenta,id_tarjeta) VALUES (1,1);
 INSERT INTO tbl_cuenta_tarjeta(id_cuenta,id_tarjeta) VALUES (2,2);
