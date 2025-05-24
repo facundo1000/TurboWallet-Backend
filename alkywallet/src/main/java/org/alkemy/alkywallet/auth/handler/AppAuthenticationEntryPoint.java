9b1fe6533005b6487bda4ca05c2546ecf13cefdc
facundo1000 <martinez.facundo85@gmail.com>
2025-05-24 16:42:35 -0300

    add: entry point config

---
+++ alkywallet/src/main/java/org/alkemy/alkywallet/auth/handler/AppAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package org.alkemy.alkywallet.auth.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AppAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        if (!response.isCommitted()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            response.getWriter().println(authException.getLocalizedMessage());
+        }
+    }
+}
