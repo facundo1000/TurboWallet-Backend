34ca6e451589bb63e8bf2dd2f0f6ed34711d52f2
facundo1000 <martinez.facundo85@gmail.com>
2025-05-28 12:59:00 -0300

    add: validations

--- alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/TarjetaDto.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/controllers/dto/TarjetaDto.java
@@ -1,5 +1,7 @@
 package org.alkemy.alkywallet.controllers.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.alkemy.alkywallet.utils.MarcaTarjeta;
 import org.alkemy.alkywallet.utils.TipoTarjeta;
 
@@ -7,15 +9,24 @@ import java.time.LocalDate;
 import java.util.Set;
 
 
-public record TarjetaDto(String nombreTitular,
-                         String cvv,
-                         String banco,
-                         String numeroTarjeta,
-                         TipoTarjeta tipo,
-                         String topeGasto,
-                         LocalDate fechaVencimiento,
-                         MarcaTarjeta marca,
-                         Boolean estado,
-                         Set<TransferenciaDto> transferenciaDto
+public record TarjetaDto(
+        @NotBlank
+        String nombreTitular,
+        @NotBlank
+        String cvv,
+        @NotBlank
+        String banco,
+        @NotBlank
+        String numeroTarjeta,
+        @NotNull
+        TipoTarjeta tipo,
+        @NotBlank
+        String topeGasto,
+        @NotNull
+        LocalDate fechaVencimiento,
+        @NotNull
+        MarcaTarjeta marca,
+        Boolean estado,
+        Set<TransferenciaDto> transferenciaDto
 ) {
 }
