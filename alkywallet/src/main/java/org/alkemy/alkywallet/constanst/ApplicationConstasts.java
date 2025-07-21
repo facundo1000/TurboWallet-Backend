1b33974903d732e22e8744e32cea27d4b7bc82e6
facundo1000 <martinez.facundo85@gmail.com>
2025-07-21 19:35:46 -0300

    documentation

--- alkywallet/src/main/java/org/alkemy/alkywallet/constanst/ApplicationConstasts.java
+++ alkywallet/src/main/java/org/alkemy/alkywallet/constanst/ApplicationConstasts.java
@@ -1,5 +1,9 @@
 package org.alkemy.alkywallet.constanst;
 
+/**
+ * This class contains constants used throughout the application.
+ */
+
 public class ApplicationConstasts {
     public static final String BASE_URL = "http://localhost:8080/api/v1/";
     public static final String PASS_REGEX = "^[A-Za-z0-9!@#$%^&*()_+\\-=\\[\\]{};':|,.<>\\/?]{4,25}$";
